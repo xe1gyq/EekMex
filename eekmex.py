8a39e4d00ea2b9a6f06c0a9ee7cf8a657e82b455
Abraham Arce <xe1gyq@gmail.com>
2015-04-26 00:15:38 -0500

    EekMex: Main Base Code
    Base code for EekMex

---
+++ eekmex.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import argparse
+
+if __name__=='__main__':
+
+	parser = argparse.ArgumentParser(description='EekMex, Amateur Radio Satellite Learning Platform')
+	args = parser.parse_args()
+
+# End of File
