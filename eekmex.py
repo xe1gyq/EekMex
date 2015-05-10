369a36c07f008e8e2184d4299ee82c9d3c16076e
Abraham Arce <xe1gyq@gmail.com>
2015-05-10 18:28:35 +0000

    EekMex: Logging Console & File
    Log both to console and a file
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- eekmex.py
+++ eekmex.py
@@ -3,14 +3,29 @@
 import argparse
 import logging
 
+def eekMexLogging():
+
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                        #datefmt='%m-%d %H:%M',
+                        filename='/home/root/eekmex/eekmex.log',
+                        filemode='w')
+
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
+
+    logging.info('EekMex Logging')
+
 if __name__=='__main__':
 
     parser = argparse.ArgumentParser(description='EekMex, Amateur Radio Satellite Learning Platform')
     args = parser.parse_args()
 
-    logging.basicConfig(filename='/home/root/eekmex/eekmex.log',level=logging.DEBUG)
-    logging.info('EekMex Logging')
-
+    eekMexLogging()
     while True:
         pass
 
