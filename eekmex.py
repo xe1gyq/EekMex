10d4b7cd8a7eb0030ff8667b7ca7157ed1ca6f2e
Abraham Arce <xe1gyq@gmail.com>
2015-05-10 18:13:27 +0000

    EekMex: Logging Module +
    We are not sure if this is the best way to handle logging module but
    incorporate it for now, also add a infinite loop
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- eekmex.py
+++ eekmex.py
@@ -1,10 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
 import argparse
+import logging
 
 if __name__=='__main__':
 
-	parser = argparse.ArgumentParser(description='EekMex, Amateur Radio Satellite Learning Platform')
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='EekMex, Amateur Radio Satellite Learning Platform')
+    args = parser.parse_args()
+
+    logging.basicConfig(filename='/home/root/eekmex/eekmex.log',level=logging.DEBUG)
+    logging.info('EekMex Logging')
+
+    while True:
+        pass
 
 # End of File
