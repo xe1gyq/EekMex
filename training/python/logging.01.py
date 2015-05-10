ef19d0c4a95a2495f5546942938c817860c90721
Abraham Arce <xe1gyq@gmail.com>
2015-05-10 15:30:07 +0000

    Training: Python Logging Modules
    Training files for Logging module
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ training/python/logging.01.py
@@ -0,0 +1,5 @@
+import logging
+
+logging.warning('EekMex Warning!')
+logging.info('EekMex Info')
+
---
+++ training/python/logging.02.py
@@ -0,0 +1,6 @@
+import logging
+
+logging.basicConfig(filename='eekmex.log', level=logging.DEBUG)
+logging.debug('Debug Message to the log file')
+logging.info('Info Message to the log file')
+logging.warning('Warning Message to the log file')
