12059a592e36f2bb37a434f3abfc090ea323b36e
root <root@ubilinux.(none)>
2015-05-17 13:27:21 +0000

    Core: Bpta Base Code
    Barometric Pressure Temperature Altitude Case Code
    
    Signed-off-by: root <root@ubilinux.(none)>
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ core/bpta.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import logging
+import getopt
+import math
+import sys
+import time
+
+import pyupm_bmpx8x as upmBmpx8x
+
+class Bpta(object):
+
+    def __init__(self):
+
+        logging.info('Barometric Pressure Temperature Altitude')
+
+        self.myBarometer = upmBmpx8x.BMPX8X(1, upmBmpx8x.ADDR);
+
+    def data(self):
+
+        while True:
+            bptadata = ("Pressure {0} | "
+                         "Temperature {1} | "
+                         "Altitude {2} | "
+                         "Sealevel {3}".format(
+                         self.myBarometer.getPressure(),
+                         self.myBarometer.getTemperature(),
+                         self.myBarometer.getAltitude(),
+                         self.myBarometer.getSealevelPressure()))
+
+            logging.info(bptadata)
+            time.sleep(.1)
+
+# End of File
