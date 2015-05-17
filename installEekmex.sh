e26eee1fcc48e65c020d7ae4c7094f7f51cd6bd3
Abraham Arce <xe1gyq@gmail.com>
2015-05-17 04:32:09 +0000

    EekMex: BMP108 & UPM
    Install UPM and Adafruit BMP108 Python code
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- installEekmex.sh
+++ installEekmex.sh
@@ -3,4 +3,15 @@
 apt-get update
 pip install numpy psutil
 
+# Adafruit BMP108 Python
+git clone https://github.com/adafruit/Adafruit_Python_BMP.git
+cd Adafruit_Python_BMP
+python setup.py install
+
+# UPM
+wget http://www.cmake.org/files/v3.2/cmake-3.2.2.tar.gz
+tar xvf cmake-3.2.2.tar.gz
+cd cmake-3.2.2
+./bootstrap && make && make install
+
 # End of File
