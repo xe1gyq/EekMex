e4176366dfcf5368dd81ff23a3b0b2751a7a6838
Abraham Arce <xe1gyq@gmail.com>
2015-05-17 06:35:34 +0000

    EekMex: Installation Upm Candidate
    Add dependencies and Upm candidate via git repository
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- installEekmex.sh
+++ installEekmex.sh
@@ -9,9 +9,23 @@ cd Adafruit_Python_BMP
 python setup.py install
 
 # UPM
+
 wget http://www.cmake.org/files/v3.2/cmake-3.2.2.tar.gz
 tar xvf cmake-3.2.2.tar.gz
 cd cmake-3.2.2
 ./bootstrap && make && make install
+
+git clone https://github.com/swig/swig.git
+./autogen.sh
+./configure
+make
+make install
+
+git clone https://github.com/intel-iot-devkit/upm.git
+mkdir build
+cd build
+cmake .. -DBUILDSWIGNODE=OFF
+make
+make install
 
 # End of File
