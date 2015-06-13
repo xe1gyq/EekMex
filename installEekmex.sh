fa58aaf85350fb5b8aca45b90b48a7aa36c2d6fe
Abraham Arce <xe1gyq@gmail.com>
2015-06-12 21:00:23 -0500

    Installation: GPS
    GPS related packages via apt

--- installEekmex.sh
+++ installEekmex.sh
@@ -3,7 +3,7 @@
 # Ubilinux
 
 apt-get update
-pip install numpy psutil XBee pyserial
+pip install numpy psutil XBee pyserial gpsd gpsd-clients python-gps
 
 # Adafruit BMP108 Python
 git clone https://github.com/adafruit/Adafruit_Python_BMP.git
