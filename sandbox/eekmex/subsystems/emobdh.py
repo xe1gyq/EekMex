e8d8b9d6baa1faa5bfb26bb3e4ee2f610804f79f
Abraham Arce <xe1gyq@gmail.com>
2015-10-18 18:38:31 +0000

    EekMex: On Board Data Handling
    OBDH Calls
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- sandbox/eekmex/subsystems/emobdh.py
+++ sandbox/eekmex/subsystems/emobdh.py
@@ -23,9 +23,8 @@ class emObdh(object):
         self.emimu = emImu()
         self.emtelemetry = emTelemetry()
 
-        #thread = Thread(target=self.emObdhRefresh)
-        #thread.start()
-        self.emObdhRefresh()
+        thread = Thread(target=self.emObdhRefresh)
+        thread.start()
 
     def emObdhSensors(self):
         altitude = emAltitudeGet()
@@ -49,20 +48,24 @@ class emObdh(object):
     def emObdhRefresh(self):
 
         altitude, pressure, sealevelpressure, temperature = self.emObdhSensors()
-        sensorsdata = ("Sensors: {0}," "{1}," "{2}," "{3}".format(altitude, pressure, sealevelpressure, temperature))
+        sensorsdata = ("Sensors: {0}," "{1}," "{2}," "{3}".format( \
+                        altitude, pressure, sealevelpressure, temperature))
         logging.info(sensorsdata)
 
         latitude, longitude, altitude =  self.emObdhGps()
-        gpsdata = ("Gps: {0}," "{1}," "{2}".format(latitude, longitude, altitude))
+        gpsdata = ("Gps: {0}," "{1}," "{2}".format( \
+                    latitude, longitude, altitude))
         logging.info(gpsdata)
 
         roll, pitch, yaw = self.emObdhImu()
         imudata = ("Imu: {0}," "{1}," "{2},".format(roll, pitch, yaw))
         logging.info(imudata)
 
-        datage = ("{0} " "{1} " "{2} " "{3} " "{4} " "{5} " "{6} " "{6} ".format(latitude, longitude, altitude, pressure, temperature, roll, pitch, yaw))
+        datage = ("{0} " "{1} " "{2} " "{3} " \
+                  "{4} " "{5} " "{6} " "{6} ".format(
+                  latitude, longitude, altitude, pressure, \
+                  temperature, roll, pitch, yaw))
         logging.warning(datage)
-
         self.emtelemetry.emTelemetrySend(datage)
 
 # End of File
--- sandbox/eekmex/subsystems/emtelemetry.py
+++ sandbox/eekmex/subsystems/emtelemetry.py
@@ -9,7 +9,6 @@ class emTelemetry(object):
     def __init__(self):
 
         logging.info('Telemetry')
-
         self.serial = serial.Serial('/dev/ttyUSB0', 9600)
 
     def emTelemetrySend(self, data):
