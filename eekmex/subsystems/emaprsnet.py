ee8aafa3c157b0f800e60b47ff55be2ecdcc6d7a
Abraham Arce <xe1gyq@gmail.com>
2015-10-18 22:43:28 +0000

    Cor: Aprs Net Base Code
    Base code for APRS Net
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ eekmex/subsystems/emaprsnet.py
@@ -0,0 +1,54 @@
+import ConfigParser
+import logging
+import string
+import sys
+from socket import *
+
+class EmAprsNet(object):
+
+    def __init__(self):
+
+        self.configuration()
+
+    def configuration(self):
+        self.conf = ConfigParser.ConfigParser()
+        self.services = "configuration/services.config"
+        self.conf.read(self.services)
+        self.server = self.conf.get("aprsnet", "server")
+        self.port = self.conf.get("aprsnet", "port")
+        self.user = self.conf.get("aprsnet", "user")
+        self.password = self.conf.get("aprsnet", "password")
+        self.address = self.conf.get("aprsnet", "address")
+        self.path = self.conf.get("aprsnet", "path")
+        self.position = self.conf.get("aprsnet", "position")
+        self.position_set(self.position)
+
+    def server_open(self):
+        self.socketid = socket(AF_INET, SOCK_STREAM)
+        self.socketid.connect((self.server, int(self.port)))
+        self.socketid.send('user ' + self.user + ' pass ' + self.password + ' vers "Experimental Project" \n')
+
+    def server_close(self):
+        self.socketid.shutdown(0)
+        self.socketid.close()
+
+    def address_set(self, address):
+        self.address = address.upper()
+
+    def position_set(self, position):
+        self.position = '=' + position + '-'
+
+    def send_message(self, message):
+        self.server_open()
+        packet = self.address + self.path + self.position + message +'\n'
+        self.socketid.send(packet)
+        logging.info(packet)
+        self.server_close()
+
+    def send_packet(self, packet):
+        self.server_open()
+        self.socketid.send(packet +'\n')
+        logging.info(packet)
+        self.server_close()
+
+# End of File
