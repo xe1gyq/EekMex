fdbf47f21f17a38d80f3505a72ddfd608d9834fd
Abraham Arce <xe1gyq@gmail.com>
2015-10-18 18:40:33 +0000

    EekMex: Code Removal
    We do not use some code, remove it
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- sandbox/eekmex/eekmex.py
+++ sandbox/eekmex/eekmex.py
@@ -73,19 +73,6 @@ if __name__=='__main__':
             obdh.emObdhRefresh()
             time.sleep(1)
 
-    sys.exit(0)
-    imu = emImu()
-    #remote = Remote()
-    #threadimu = threading.Thread(name='imu', target=imu.data)
-    #threadimu.daemon = True
-    #threadimu.start()
-    seconds = 0
-    while seconds != 5:
-        time.sleep(1)
-        seconds += 1
-        #remote.send()
-    #remote.close()
-
 if __name__ == "__main__":
 
     main(sys.argv[1:])
