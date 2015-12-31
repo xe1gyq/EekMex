faf4b4f674d55ee0cb54e2afa0b01dd77f73ff22
Abraham Arce <abraham.arce.moreno@intel.com>
2015-12-31 14:31:10 -0600

    Eekmex: Training Removal
    Why Traning? Remove it!
    
    Signed-off-by: Abraham Arce <abraham.arce.moreno@intel.com>

--- training/python/logging.01.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-logging.warning('EekMex Warning!')
-logging.info('EekMex Info')
-
--- training/python/logging.02.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-logging.basicConfig(filename='eekmex.log', level=logging.DEBUG)
-logging.debug('Debug Message to the log file')
-logging.info('Info Message to the log file')
-logging.warning('Warning Message to the log file')
